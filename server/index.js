06ef7d02e1bb6ef1bdda279bc396fe4fe379db43
Anna Dinh <Annathuydinh@gmail.com>
2020-05-01 13:24:31 -0700

    Set up express connection with our static route and our CRUD routes

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const router = require('./router.js');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use('/products', router);
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
