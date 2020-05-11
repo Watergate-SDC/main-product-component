3d461b5658782c937ee74fca4866434a4587e89c
Anna Dinh <Annathuydinh@gmail.com>
2020-05-11 10:43:58 -0700

    created two functions that gets cart information (total and num of items) and an add to cart function

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -12,5 +12,30 @@ module.exports = {
         db.query(queryStr, (err, results) => {
             err ? callback(err) : callback(null, results);
         })
+    },
+    getCartInfo: (callback) => {
+        let queryStr = `SELECT * FROM cart;`;
+        db.query(queryStr, (err, results) => {
+            if (err) {
+                callback(err)
+            } else { 
+                let information = {
+                    subtotal: 0,
+                    totalItems: 0
+                }
+                // results is an array 
+                for (let i = 0; i < results.length; i++) {
+                    information.subtotal += results[i].price;
+                    information.totalItems += 1;
+                }
+                callback(null, information);
+            }
+        })
+    },
+    addToCart: (price, callback) => {
+        let queryStr = `INSERT INTO cart (price) VALUES (${price});`;
+        db.query(queryStr, (err, results) => {
+            err ? callback(err) : callback(null, results);
+        })
     }
 };
