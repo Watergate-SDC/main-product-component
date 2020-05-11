24ed560bd36ac0f2ebc2e58b2e45d78edee49ae3
Anna Dinh <Annathuydinh@gmail.com>
2020-05-11 10:45:38 -0700

    created a new route to /cart and has a get and post

--- server/router.js
+++ server/router.js
@@ -2,12 +2,21 @@ const router = require('express').Router();
 const controller = require('./controller.js');
 
 router
-    .route('/')
-    .get(controller.getAll);
-
+    .route('/products')
+    // gets all 100 product info
+    .get(controller.getAll)
+    
+router
+    .route('/cart')
+    // gets subtotal of cart
+    .get(controller.getCartInfo)
+    // adds a product to cart
+    .post(controller.addToCart);
 router
     .route('/:id')
+    // gets one product info
     .get(controller.getOne);
+    
 
 // add to cart
 // post
