2470c05d91dc30303e8a2db44c775cc3e0e46662
Anna Dinh <Annathuydinh@gmail.com>
2020-05-11 10:41:19 -0700

    Added separate AddToBag component

--- client/src/components/AddToBag.jsx
+++ client/src/components/AddToBag.jsx
@@ -1,15 +1,28 @@
-// import React from 'react';
+import React from 'react';
 
-// class AddToBag extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
+class AddToBag extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            totalItems: 0,
+            totalPrice: 0
+        }
+    }
+    componentDidMount() {
 
-//         }
-//     }
-//     render() {
+    }
 
-//     }
-// }
+    
+    render() {
+        return (
+            <div className="purchase-method__buttons">
+                <button className="add-to-bag" onClick={this.addToBag} type="button">Add to Bag</button>
+                <div className="free-shipping-and-free-returns">
+                    <p className="lll-text-body-1">Free Shipping and Free Returns</p>
+                </div>
+            </div>
+        )
+    }
+}
 
-// export default AddToBag;
+export default AddToBag;
