83d25fed3c89125d0aeb506828b5fc8fdc09deb5
Anna Dinh <Annathuydinh@gmail.com>
2020-05-11 10:44:39 -0700

    controller functions for getCartInfo and addToCart

--- server/controller.js
+++ server/controller.js
@@ -11,6 +11,18 @@ const controller = {
         helper.getOne(id, (err, results) => {
             err ? res.status(404).send(err) : res.status(200).json(results);
         })
+    },
+    getCartInfo: (req, res) => {
+        helper.getTotalPrice((err, results) => {
+            err ? res.status(404).send(err) : res.status(200).json(results);
+        })
+    },
+    addToCart: (req, res) => {
+        let {price} = req.body;
+        helper.addToCart(price, (err, results) => {
+            err ? res.status(404).send('Unable to add to cart') : res.status(200).send('Product added to cart.');
+        })
+
     }
 }
 
