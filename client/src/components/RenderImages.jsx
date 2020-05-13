597d7a1ed7a75eeba96f9df70287706287c7c5ed
Anna Dinh <Annathuydinh@gmail.com>
2020-05-13 00:53:24 -0700

    Removed Pure-React-Carousel library and replaced it with BootStrap's carousel

--- client/src/components/RenderImages.jsx
+++ client/src/components/RenderImages.jsx
@@ -1,53 +1,31 @@
-import React from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image, Dot } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
 
 const RenderImages = ({ currentColors }) => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
     <div className='pdp-carousel-images-wrapper-content'>
-      <CarouselProvider
-        naturalSlideWidth={42}
-        naturalSlideHeight={50}
-        totalSlides={2}
-        dragStep={1}
-        currentSlide={0}
-        className='product-image-carousel'
-      >
-        <div className='swiper-container-horizontal'>
-          <Slider>
-            <Slide index={0} id='carousel__slide'>
-              <Image hasMasterSpinner='true'>
-                <img className='product-image-1' 
-                  src={currentColors.image1}>
-                </img>
-              </Image>
-            </Slide>
-            <Slide index={1} id='carousel__slide'>
-              <Image hasMasterSpinner='true'>
-                <img className='product-image-2'
-                  src={currentColors.image2}>
-                </img>
-              </Image>
-            </Slide>
-          </Slider>
-          <div className='dots'>
-            <Dot slide={0} id='dot' disabled='null'>
-            </Dot>
-            <Dot slide={1} id='dot' disabled='null'>
-            </Dot>
-          </div>
-          <ButtonBack className='arrows'>
-            <Image>
-              <img className='left-arrow' src='https://lulupics.s3.us-east-2.amazonaws.com/Arrows/Left.jpg' />
-            </Image>
-          </ButtonBack>
-          <ButtonNext className='arrows'>
-            <Image>
-              <img className='right-arrow' src='https://lulupics.s3.us-east-2.amazonaws.com/Arrows/Right.jpg' />
-            </Image>
-          </ButtonNext>
-        </div>
-      </CarouselProvider>
+      <Carousel activeIndex={index} onSelect={handleSelect} interval='1000000'>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={currentColors.image1}
+            alt='First slide'
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src={currentColors.image2}
+            alt='Second slide'
+          />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
